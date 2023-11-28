8a790e6618e7b577377060325f7bfa3a11321c5e
JussyJ <48039566+JussyJ@users.noreply.github.com>
2023-11-28 11:23:42 +0800

    Add files via upload

---
+++ public/scripts/config.js
@@ -0,0 +1,18 @@
+export default {
+    // Token Implicit Grant Client ID
+    clientID: '360513bd-fe43-46e7-bb79-b7fb3ae38f62',
+
+    // redirectUri: 'https://translatev2.azurewebsites.net/translator',
+    redirectUri: 'http://localhost:3001/message',
+
+    genesysCloud: {
+        // Genesys Cloud region
+        // eg. 'mypurecloud.ie', 'euw2.pure.cloud', etc...
+        // region: 'mypurecloud.com'
+        region: 'usw2.pure.cloud'
+    },
+
+    // translateServiceURI: 'https://translatev2.azurewebsites.net/translate'
+    translateServiceURI: 'http://localhost:3001/translate'
+} 
+
